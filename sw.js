88759e8a80f4f2dead618e841823cbaf4438e8b3
Geraldo Xexeo <gxexeo@gmail.com>
2026-09-21 09:25:33 -0300

    Refresh PWA cache after execution fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="processpanic-v0.2.0-r2";
+const CACHE="processpanic-v0.2.0-r3";
 const ASSETS=["./","./index.html","./article.html","./play/","./play/index.html","./processpanic.html","./manifest.webmanifest","./icons/icon.svg","./icons/icon-192.png","./icons/icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
